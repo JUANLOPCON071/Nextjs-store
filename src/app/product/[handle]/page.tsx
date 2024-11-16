0b7069b58b7224e661efa02fae9739876a351a65
juanlopcon07 <juan.21futmas07@gmail.com>
2024-11-15 22:31:08 -0500

    Redireccion y ProductView

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,5 +1,6 @@
 import { ProductView } from 'app/components/product/ProductView/ProductView'
 import { getProducts } from 'app/services/shopify/products'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 interface ProductPageProps {
@@ -12,6 +13,10 @@ const ProductPage = async ({ searchParams }: ProductPageProps) => {
     const id = searchParams.id
     const products = await getProducts(id)
     const product = products[0]
+
+    if(!id){
+      redirect('/store')
+    }
     
   return (
     <ProductView product={product}/>
