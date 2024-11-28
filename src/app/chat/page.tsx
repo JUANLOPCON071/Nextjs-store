db4476f08af301508606472770dd908849214e2a
juanlopcon07 <juan.21futmas07@gmail.com>
2024-11-28 08:07:23 -0500

    middleware para proteger rutas, chatbot con SDK IA de vercel, bot de ventas

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,18 @@
+import { Chat } from "app/components/chat/chat"
+import { getProducts } from "app/services/shopify/products"
+import { createAgent } from "app/utils/openai/createAgent"
+
+export default async function ChatPage() {
+
+  const products = await getProducts()
+  const productTitles = products.map((product) => product.title)
+  const flatProductsTitles = productTitles.join("\n")
+  const agent = createAgent(flatProductsTitles)
+
+    return (
+        <>
+          <h1>Chatbot</h1>
+          <Chat agent={agent}/>
+        </>
+    )
+}
--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,23 +1,23 @@
 import { GraphQLClient } from 'graphql-request';
-import { env } from "app/config/env";
+import { env } from 'app/config/env';
 
 export class GraphQLClientSingleton {
-    private readonly endpoint: string = env.SHOPIFY_GRAPHQL_ENDPOINT;
+  private readonly endpoint: string = env.SHOPIFY_GRAPHQL_ENDPOINT;
 
-    private static instance: GraphQLClientSingleton;
+  private static instance: GraphQLClientSingleton;
 
-    static getInstance(): GraphQLClientSingleton {
-        if(!this.instance) {
-            this.instance = new GraphQLClientSingleton();
-        }
-        return this.instance;
+  static getInstance(): GraphQLClientSingleton {
+    if (!this.instance) {
+      this.instance = new GraphQLClientSingleton();
     }
+    return this.instance;
+  }
 
-    getClient():GraphQLClient {
-        return new GraphQLClient(this.endpoint, {
-            headers: {
-                'Shopify-Storefront-Private-Token': env.SHOPIFY_STOREFRONT_TOKEN,
-            },
-        })
-    }
+  getClient(): GraphQLClient {
+    return new GraphQLClient(this.endpoint, {
+      headers: {
+        'Shopify-Storefront-Private-Token': env.SHOPIFY_STOREFRONT_TOKEN,
+      },
+    });
+  }
 }
